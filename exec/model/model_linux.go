ea2657a747e357bf5262b5a3d3ae8f70754d7d78
keke <keke>
2023-02-09 17:14:28 +0800

    feat: update model_linux

--- exec/model/model_linux.go
+++ exec/model/model_linux.go
@@ -20,6 +20,7 @@ import (
 	"github.com/chaosblade-io/chaosblade-exec-os/exec/cpu"
 	"github.com/chaosblade-io/chaosblade-exec-os/exec/disk"
 	"github.com/chaosblade-io/chaosblade-exec-os/exec/file"
+	"github.com/chaosblade-io/chaosblade-exec-os/exec/http"
 	"github.com/chaosblade-io/chaosblade-exec-os/exec/kernel"
 	"github.com/chaosblade-io/chaosblade-exec-os/exec/mem"
 	"github.com/chaosblade-io/chaosblade-exec-os/exec/network"
